94fe399e970bacb94a16fe7ce839ad5675f7b116
tdesef <thorsten.desef@online.de>
2020-03-04 21:20:55 +0100

    initial

---
+++ webapp/controller/NotFound.controller.js
@@ -0,0 +1,24 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("student00.sap.training.dynamicpage.controller.NotFound", {
+		getRouter: function () {
+			return sap.ui.core.UIComponent.getRouterFor(this);
+		},
+
+		onNavBack: function () {
+			var oHistory, sPreviousHash, oRouter;
+			oHistory = sap.ui.core.routing.History.getInstance();
+			sPreviousHash = oHistory.getPreviousHash();
+
+			if (sPreviousHash !== undefined) {
+				window.history.go(-1);
+			} else {
+				oRouter = this.getRouter();
+				oRouter.navTo("Overview", true /*no history*/ );
+			}
+		}
+	});
+});
